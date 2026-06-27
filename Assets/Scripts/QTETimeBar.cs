9bf947dea24ab1e359bd834375a6922305fa380c
Gaz Dragon <kimhapp.huy@gmail.com>
2026-06-27 11:59:07 +0700

    sync to laptop

--- Assets/Scripts/QTETimeBar.cs
+++ Assets/Scripts/QTETimeBar.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Slider))]
 public class QTETimeBar : MonoBehaviour
 {
+    Slider slider;
+
+    public float direction = 1f;
+    public float speed = 1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slider.value += direction * speed * Time.deltaTime;
+
+        if (slider.value >= slider.maxValue)
+        {
+            direction = -1f;
+        }
+        else if (slider.value <= slider.minValue)
+        {
+            direction = 1f;
+        }
     }
 }

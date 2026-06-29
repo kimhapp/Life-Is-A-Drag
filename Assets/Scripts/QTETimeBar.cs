90e02efc9cb8f0a18f11d037b7fe478afa600fcd
Gaz Dragon <kimhapp.huy@gmail.com>
2026-06-29 08:34:28 +0700

    sync to laptop

--- Assets/Scripts/QTETimeBar.cs
+++ Assets/Scripts/QTETimeBar.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Slider))]
 public class QTETimeBar : MonoBehaviour
 {
-    Slider slider;
+    [SerializeField] InputActionReference qteLeftAction;
+    [SerializeField] InputActionReference qteRightAction;
+    [SerializeField] float direction = 1f;
+    [SerializeField] float speed = 1f;
 
-    public float direction = 1f;
-    public float speed = 1f;
+    Slider slider;
+    Vector2 safezoneMinMax = new(0.45f, 0.55f);
+
+    void Awake()
+    {
+        qteLeftAction.action.Enable();
+        qteRightAction.action.Enable();
+
+        qteLeftAction.action.started += TurnSliderLeft;
+        qteRightAction.action.started += TurnSliderRight;
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         slider = GetComponent<Slider>();
+
+        // Debug enabled state
+        Debug.Log($"Move action enabled: {qteLeftAction.action.enabled}");
+        Debug.Log($"Interact action enabled: {qteRightAction.action.enabled}");
     }
 
     // Update is called once per frame
@@ -28,5 +45,22 @@ public class QTETimeBar : MonoBehaviour
         {
             direction = 1f;
         }
+
+        if (slider.value >= safezoneMinMax.x && slider.value <= safezoneMinMax.y)
+        {
+            Debug.Log("In safezone!");
+        }
+    }
+
+    void TurnSliderLeft(InputAction.CallbackContext callback)
+    {
+        Debug.Log("left!");
+        direction = -1f;
+    }
+
+    void TurnSliderRight(InputAction.CallbackContext callback)
+    {
+        Debug.Log("right!");
+        direction = 1f;
     }
 }
